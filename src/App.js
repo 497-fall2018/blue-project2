855d21eda825f85a1f0ad0c5309972911e4888a3
Robbie Belson <robbiebelson@dhcp-10-105-232-200.wireless.northwestern.private>
2018-10-15 10:59:44 -0500

    first commit for DJPrudp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import app_logo from './app_logo.png';
+import music from './autumn_leaves.mp3';
+import $ from 'jquery'; 
+// import Bloodhound from './typeahead.js/bloodhound.js';
 
 class App extends Component {
   render() {
+    
+    // var activities = new Bloodhound({
+    // datumTokenizer: Bloodhound.tokenizers.whitespace,
+    // queryTokenizer: Bloodhound.tokenizers.whitespace,
+    //   prefetch: './activities.json'
+    // });
+
+    // $('#prefetch .typeahead').typeahead(null, {
+    //   name: 'activities',
+    //   source: activities
+    // });
+
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className='App'>
+        <header id="app_header">
+          <img id="logo" src={app_logo} alt="logo" width="200"/>
+              <h1 id="app_title"> Welcome to DJPrudo!</h1>
+              <h4>Your Home For Personal Music Curation</h4>
+                <audio autoPlay loop>
+                <source src={music}/>
+                </audio>
         </header>
+        
+
+        <div className='container'>
+          <section className='add-item'>
+              <form id="form" action="#">
+              <div id="prefetch">
+                <input id='add_activity' class="typeahead" type="text" placeholder="Enter Your Activity Here"/>
+              </div>
+
+              <button id="generate_music" onClick={function() {document.getElementById('rec').hidden=false;}}>Generate Music</button>
+              </form>
+          </section>
+
+          <div hidden id="rec" class="w3-third w3-margin-bottom">
+          <div class="w3-container w3-white">
+            <p><b>Your Recommendation: Classical Music</b></p>
+            <p class="w3-opacity">Robert Belson plays Classical Guitar</p>
+            <iframe width="75%" height="400" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/281582773&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+          </div>
+        </div>
+
+
+
+
+        </div>
       </div>
+
     );
   }
 }
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAXh8LjJ84jE_70xEXtC1xNUa7TnPmX0MA",
+    authDomain: "dj-prudo.firebaseapp.com",
+    databaseURL: "https://dj-prudo.firebaseio.com",
+    projectId: "dj-prudo",
+    storageBucket: "",
+    messagingSenderId: "581616322169"
+  };
+  firebase.initializeApp(config);
+  export default firebase;
