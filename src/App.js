4658ee5766e6119bb914b81fb51d70e14baf31c3
Robbie Belson <robbiebelson@dhcp-10-105-232-200.wireless.northwestern.private>
2018-10-17 10:04:22 -0500

    added connection to firebase

--- src/App.js
+++ src/App.js
@@ -2,21 +2,70 @@ import React, { Component } from 'react';
 import './App.css';
 import app_logo from './app_logo.png';
 import music from './autumn_leaves.mp3';
+import firebase from './firebase.js';
 import $ from 'jquery'; 
 import {Typeahead} from 'react-bootstrap-typeahead'; // ES2015
 import 'react-bootstrap-typeahead/css/Typeahead.css';
+import { Button } from 'reactstrap';
+
 
 
 class App extends Component {
-  
-  state = {
-      multiple: false,
-    };
+  constructor() {
+    super();
+    this.state={
+      activity:'',
+      activity_src:'',
+      playlists: []
+    } 
+    this.handleChange=this.handleChange.bind(this);
+    this.handleSubmit=this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]:e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const playlistsRef=firebase.database().ref('playlists');
+    const playlist = {
+      activity: this.state.activity,
+      activity_src: this.state.activity_src
+    }
+    playlistsRef.push(playlist);
+    this.setState({
+      activity:'',
+      activity_src:'',
+    });
+  }
+
+
+  componentDidMount() {
+    const playlistsRef = firebase.database().ref('playlists');
+    playlistsRef.on('value', (snapshot) => {
+      let playlists = snapshot.val();
+      let newState = [];
+      for (let p in playlists) {
+        newState.push({
+          id: p,
+          activity: playlists[p].activity,
+          activity_src: playlists[p].activity_src
+        });
+      }
+      this.setState({
+        playlists: newState
+      });
+    });
+    }
+
 
 
   render() {
-    var Typeahead = require('react-bootstrap-typeahead').Typeahead; // CommonJS
-    const {multiple} = this.state;
+    var user_requested_activity="";
+    var requested_url="";
 
     return (
       <div className='App'>
@@ -34,14 +83,11 @@ class App extends Component {
           <section className='add-item'>
               <form id="form" action="#">
               <div id="prefetch">
-                <input id='add_activity' class="typeahead" type="text" placeholder="Enter Your Activity Here"/>
+                <input id='add_activity' class="typeahead" type="text" placeholder="Enter Your Activity Here" onChange={this.handelChange}/>
               </div>
 
            
-
-
-
-              <button id="generate_music" onClick={function() {document.getElementById('rec').hidden=false;}}>Generate Music</button>
+              <button id="generate_music" onClick={function() {user_requested_activity=document.getElementById('add_activity').innerHTML; requested_url=this.playlists[user_requested_activity]; document.getElementById('user_playlist').src=requested_url; document.getElementById('rec').hidden=false;}}>Generate Music</button>
               </form>
           </section>
 
@@ -49,7 +95,7 @@ class App extends Component {
           <div class="w3-container w3-white">
             <p><b>Your Recommendation: Classical Music</b></p>
             <p class="w3-opacity">Robert Belson plays Classical Guitar</p>
-            <iframe width="75%" height="400" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/281582773&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+            <iframe id="user_playlist" width="75%" height="400" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/281582773&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
           </div>
         </div>
 
