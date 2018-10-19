f2fb746b136fb1ab3558e527bf0044df2e3d2991
Puno.Lee <punolee@outlook.com>
2018-10-19 17:06:35 -0500

    added a searchbtn component and some material-ui stuff

--- src/App.js
+++ src/App.js
@@ -3,28 +3,30 @@ import './App.css';
 import app_logo from './app_logo.png';
 import music from './autumn_leaves.mp3';
 import firebase from './firebase.js';
-import $ from 'jquery'; 
+import $ from 'jquery';
 import { Typeahead } from 'react-bootstrap-typeahead'; // ES2015
 import 'react-bootstrap-typeahead/css/Typeahead.css';
-import { Button } from 'reactstrap';
-
-
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Searchbutton from './component/searchbutton';
 
 class App extends Component {
+
   constructor() {
+
     super();
-    this.state={
-      activity:'',
-      activity_src:'',
+    this.state = {
+      activity: '',
+      activity_src: '',
       playlists: []
-    } 
-    this.handleChange=this.handleChange.bind(this);
-    this.handleSubmit=this.handleSubmit.bind(this);
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(e) {
     this.setState({
-      activity:e.target.value,
+      activity: e.target.value,
     });
   }
 
@@ -32,16 +34,16 @@ class App extends Component {
     e.preventDefault();
     var temp_url = ''
     for (let p in this.state.playlists) {
-      if(this.state.playlists[p].activity.toUpperCase() == this.state.activity.toUpperCase()){
+      if (this.state.playlists[p].activity.toUpperCase() == this.state.activity.toUpperCase()) {
         console.log(this.state.playlists[p].activity)
-          temp_url = this.state.playlists[p].activity_src;
-          console.log(temp_url);
-          break;
+        temp_url = this.state.playlists[p].activity_src;
+        console.log(temp_url);
+        break;
 
-        }
       }
+    }
     this.setState({
-      activity_src:temp_url,
+      activity_src: temp_url,
     });
     console.log(this.state.activity_src)
   }
@@ -52,7 +54,7 @@ class App extends Component {
     playlistsRef.on('value', (snapshot) => {
       let playlists = snapshot.val();
       let newState = [];
-      
+
       for (let p in playlists) {
         newState.push({
           id: p,
@@ -63,61 +65,79 @@ class App extends Component {
 
       this.setState({
         playlists: newState,
-        
+
       });
 
     });
-    }
+  }
 
   render() {
-    var user_requested_activity="";
-    var requested_url="";
+    var user_requested_activity = "";
+    var requested_url = "";
     for (let p in this.state.playlists) {
-        if(this.state.playlists[p].activity == 'Cleaning'){
-          requested_url = this.state.playlists[p].activity_src;
-          break;
-        }
+      if (this.state.playlists[p].activity == 'Cleaning') {
+        requested_url = this.state.playlists[p].activity_src;
+        break;
       }
+    }
 
 
     return (
       <div className='App'>
-          <div className ='searchPart'>
-            <header id="app_header">
-              <img id="logo" src={app_logo} alt="logo" width="200"/>
-                  <h1 id="app_title"> Welcome to DJPrudo!</h1>
-                  <h4>Your Home For Personal Music Curation</h4>
-                    <audio autoPlay loop>
-                    <source src={music}/>
-                    </audio>
-            </header>
 
-              <section className='add-item'>
-                <form id="form" action="#" onSubmit={this.handleSubmit}>
-                <div id="prefetch">
-                      <Typeahead
-                          labelKey="name"
-                          options={["Running","Studying","Working Out", "Eating","Sleeping","Relaxing"]}
-                          placeholder="Choose an activity"
-                          />
-                <input id='add_activity' class="typeahead" type="text" placeholder="Enter Your Activity Here" onChange={this.handleChange}/>
-                </div>
-                  <button id="generate_music"
-                    onClick={
-                    function() {
-                      document.getElementById('rec').hidden=false;}}>
-                      Generate Music</button>
-                  </form>
-              </section>
-          </div>
+        <div className='searchPart'>
 
-          <div className= "RecPart">
-              <div hidden id="rec" class="w3-third w3-margin-bottom">
-                <div class="w3-container w3-white">
-                    <iframe id="user_playlist" width="75%" height="400" scrolling="no" frameborder="no" src={this.state.activity_src}></iframe>                 
+          <section className='add-item'>
+            <form id="form" action="#" onSubmit={this.handleSubmit}>
+              <div id="prefetch">
+                <div style={{ padding: 100 }}>
+                  <Grid
+                    container
+                    direction="column"
+                    justify="left"
+                    alignItems="left"
+
+                  >
+                    <Typography align='left' color='inherit' component="h2" variant="h1" gutterBottom>
+                      DJ Produ
+                    </Typography>
+                    <Typography align='left' color='inherit' variant="h5" gutterBottom>
+                      DJ Produ saves your favorite beats all across platforms. Type your task and you are ready to go!
+      </Typography>
+                    <div style={{ marginTop: 20 }}>
+
+                      <Searchbutton />
+
+
+                    </div>
+                  </Grid>
                 </div>
+
+                <Typeahead
+                  labelKey="name"
+                  options={["Running", "Studying", "Working Out", "Eating", "Sleeping", "Relaxing"]}
+                  placeholder="Choose an activity"
+                />
+                <input id='add_activity' class="typeahead" type="text" placeholder="Enter Your Activity Here" onChange={this.handleChange} />
               </div>
+
+              <button id="generate_music"
+                onClick={
+                  function () {
+                    document.getElementById('rec').hidden = false;
+                  }}>
+                Generate Music</button>
+            </form>
+          </section>
+        </div>
+
+        <div className="RecPart">
+          <div hidden id="rec" class="w3-third w3-margin-bottom">
+            <div class="w3-container w3-white">
+              <iframe id="user_playlist" width="75%" height="400" scrolling="no" frameborder="no" src={this.state.activity_src}></iframe>
+            </div>
           </div>
+        </div>
       </div>
 
     );
